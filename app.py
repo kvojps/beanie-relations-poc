a9c3b1e240800ba36fa22e16311bf7aaaf5d6108
Jose Ferreira <josebazjunior09@gmail.com>
2023-10-16 23:27:55 -0300

    [fix]: refactor posts attr in user to receive a empty list on creation

--- app.py
+++ app.py
@@ -13,9 +13,10 @@ async def start_mongo_db():
     await init_mongo_db()
 
 
-@app.post('/usuario', response_model=UserResponse, status_code=status.HTTP_201_CREATED)
+@app.post('/usuarios', response_model=UserResponse, status_code=status.HTTP_201_CREATED)
 async def create_user(user_request: UserRequest):
-    user_data = User(name=user_request.name, email=user_request.email)
+    user_data = User(name=user_request.name,
+                     email=user_request.email, posts=[])
     user_created = await user_data.save(link_rule=WriteRules.DO_NOTHING)
 
     return UserResponse(name=user_created.name, email=user_created.email, posts=user_created.posts)
--- db_config.py
+++ db_config.py
@@ -5,4 +5,4 @@ from beanie import init_beanie
 async def init_mongo_db():
     client = motor.motor_asyncio.AsyncIOMotorClient(
         "mongodb://mongo:mongo@localhost:27017/")
-    await init_beanie(database=client['logging'], document_models=['models.User', 'models.Post'])
+    await init_beanie(database=client['user'], document_models=['models.User', 'models.Post'])
