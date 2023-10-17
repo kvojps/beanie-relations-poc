ae19b3bd9d888af479c28ad87684f988c429d598
Jose Ferreira <josebazjunior09@gmail.com>
2023-10-17 00:21:59 -0300

    [feat]: add create post

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Request, status
 from beanie import WriteRules
 
 from models import User, Post
-from serializers import UserRequest, UserResponse
+from serializers import UserRequest, UserResponse, PostRequest, PostResponse
 from db_config import init_mongo_db
 
 app = FastAPI()
@@ -20,3 +20,17 @@ async def create_user(user_request: UserRequest):
     user_created = await user_data.save(link_rule=WriteRules.DO_NOTHING)
 
     return UserResponse(name=user_created.name, email=user_created.email, posts=user_created.posts)
+
+
+@app.post('/posts', response_model=PostResponse, status_code=status.HTTP_201_CREATED)
+async def create_post(post_request: PostRequest):
+    user_found: User | None = await User.get(post_request.user_id)
+    post_data = Post(title=post_request.title, content=post_request.content)
+
+    if not user_found:
+        raise ValueError("TESTE")
+
+    user_found.posts = [post_data]
+    post_created = await user_found.save(link_rule=WriteRules.WRITE)
+
+    return PostResponse(title="post_created.title", content="post_created.content", user_id="post_request.user_id")
